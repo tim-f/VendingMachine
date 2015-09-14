c1fb1a8d330cb64610a1dd0b91c35533bf57a483
Timur Fayzrakhmanov <timur.f@outlook.com>
2015-09-14 19:20:15 +0300

    #1 Navigation service contract added; Wallet page markup updated;

---
+++ src/VendingMachine.Presentation.Wpf/Navigation/INavigationService.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using JetBrains.Annotations;
+
+namespace VendingMachine.Presentation.Wpf.Navigation
+{
+    public interface INavigationService
+    {
+        void ShowUserWallet();
+        void ShowVendingMachineWallet();
+        void ShowVendingMachineMenu();
+    }
+
+    internal sealed class NavigationService : INavigationService
+    {
+        [NotNull]
+        private MainWindow MainWindow => ((MainWindow)Application.Current.MainWindow);
+
+        [NotNull]
+        private Frame UserWalletFrame => MainWindow.Wallet;
+
+        [NotNull]
+        private Frame VendingMachineWalletFrame => MainWindow.VendingMachine;
+
+
+        public void ShowVendingMachineWallet()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ShowVendingMachineMenu()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ShowUserWallet()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
