637132edb7285e11c24ed0b430186692e89123ef
Timur Fayzrakhmanov <timur.f@outlook.com>
2015-10-19 01:05:00 +0300

    TakeChange command added;

---
+++ src/VendingMachine.ApplicationLogic/Commands/TakeChangeCommand.cs
@@ -0,0 +1,24 @@
+﻿using VendingMachine.ApplicationLogic.AppModel;
+using VendingMachine.ApplicationLogic.Utility;
+using VendingMachine.Core;
+using VendingMachine.Core.Services;
+
+namespace VendingMachine.ApplicationLogic.Commands
+{
+    public class TakeChangeCommand : IChainCommand<CashDepositModel, CoinSet>
+    {
+        private IMachineOperations MachineOperations { get; }
+
+        public TakeChangeCommand(IMachineOperations machineOperations)
+        {
+            MachineOperations = machineOperations;
+        }
+
+        public CoinSet Execute(CashDepositModel parameter)
+        {
+            CoinSet change = MachineOperations.RetrieveChange();
+            parameter.Amount = decimal.Zero;
+            return change;
+        }
+    }
+}
