410343e98cb9a4aaf6d87b64c0759a496d3ec972
Timur Fayzrakhmanov <timur.f@outlook.com>
2015-10-19 01:04:14 +0300

    Machine operations refactoring;

--- src/VendingMachine.Core/Services/IMachineOperations.cs
+++ src/VendingMachine.Core/Services/IMachineOperations.cs
@@ -4,20 +4,6 @@
     {
         void DepositCoin(Coin coin);
         decimal GetDepositAmount();
-    }
-
-    public class MachineOperations : IMachineOperations
-    {
-        private IMachineWallet MachineWallet { get; } = new MachineWallet();
-
-        public void DepositCoin(Coin coin)
-        {
-            MachineWallet.DepositCoin(coin);
-        }
-
-        public decimal GetDepositAmount()
-        {
-            return MachineWallet.GetDepositAmount();
-        }
+        CoinSet RetrieveChange();
     }
 }
---
+++ src/VendingMachine.Core/Services/MachineOperations.cs
@@ -0,0 +1,22 @@
+﻿namespace VendingMachine.Core.Services
+{
+    public class MachineOperations : IMachineOperations
+    {
+        private IMachineWallet MachineWallet { get; } = new MachineWallet();
+
+        public void DepositCoin(Coin coin)
+        {
+            MachineWallet.DepositCoin(coin);
+        }
+
+        public decimal GetDepositAmount()
+        {
+            return MachineWallet.GetDepositAmount();
+        }
+
+        public CoinSet RetrieveChange()
+        {
+            return MachineWallet.RequestCashBack();
+        }
+    }
+}
