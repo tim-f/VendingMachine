0aadda9265ab0e6292bf421bfe9fe4a8015a84ee
Timur Fayzrakhmanov <timur.f@outlook.com>
2015-10-18 19:30:45 +0300

    Removed redundant constructor;

--- src/VendingMachine.Presentation.Wpf/Services.cs
+++ src/VendingMachine.Presentation.Wpf/Services.cs
@@ -9,12 +9,5 @@ namespace VendingMachine.Presentation.Wpf
         public IUserWallet UserWallet { get; } = new UserWallet();
         public IMachineOperations MachineOperations { get; } = new MachineOperations();
         public IVisualizer Visualizer { get; } = new Visualizer();
-
-        public AppServices()
-        {
-            //IUserWallet userWallet = new UserWallet();
-            //IMachineOperations machineOperations = new MachineOperations();
-            //IVisualizer visualizer = new Visualizer();
-        }
     }
 }
