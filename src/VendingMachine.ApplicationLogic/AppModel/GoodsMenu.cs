8fa6739da766fbc82899affbdf2755a3ddf6ee13
Timur Fayzrakhmanov <timur.f@outlook.com>
2015-10-18 14:08:35 +0300

    Updated GoodsMenu page and model

--- src/VendingMachine.ApplicationLogic/AppModel/GoodsMenu.cs
+++ src/VendingMachine.ApplicationLogic/AppModel/GoodsMenu.cs
@@ -6,47 +6,6 @@ namespace VendingMachine.ApplicationLogic.AppModel
     public sealed class GoodsMenu : AppModel
     {
         [NotNull]
-        public ObservableCollection<GoodsPosition> GoodsList { get; } = new ObservableCollection<GoodsPosition>();
-    }
-
-    public sealed class GoodsPosition : AppModel
-    {
-        private string _name;
-        private decimal _price;
-        private int _count;
-
-        [NotNull]
-        public string Name
-        {
-            get { return _name; }
-            set
-            {
-                if (_name == value) return;
-                _name = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public decimal Price
-        {
-            get { return _price; }
-            set
-            {
-                if (_price == value) return;
-                _price = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public int Count
-        {
-            get { return _count; }
-            set
-            {
-                if (_count == value) return;
-                _count = value;
-                OnPropertyChanged();
-            }
-        }
+        public ObservableCollection<MenuItem> MenuItems { get; } = new ObservableCollection<MenuItem>();
     }
 }
---
+++ src/VendingMachine.ApplicationLogic/AppModel/MenuItem.cs
@@ -0,0 +1,57 @@
+﻿using JetBrains.Annotations;
+
+namespace VendingMachine.ApplicationLogic.AppModel
+{
+    public sealed class MenuItem : AppModel
+    {
+        private string _productName;
+        private decimal _price;
+        private int _count;
+        private bool _isAvailable;
+
+        [NotNull]
+        public string ProductName
+        {
+            get { return _productName; }
+            set
+            {
+                if (_productName == value) return;
+                _productName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public decimal Price
+        {
+            get { return _price; }
+            set
+            {
+                if (_price == value) return;
+                _price = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int Count
+        {
+            get { return _count; }
+            set
+            {
+                if (_count == value) return;
+                _count = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool IsAvailable
+        {
+            get { return _isAvailable; }
+            set
+            {
+                if (_isAvailable == value) return;
+                _isAvailable = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
