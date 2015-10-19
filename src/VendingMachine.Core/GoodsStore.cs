b46b2d61fad88a6d408c6e889d15775bafbbae9e
Timur Fayzrakhmanov <timur.f@outlook.com>
2015-10-20 00:12:03 +0300

    Add proruct core entities;

---
+++ src/VendingMachine.Core/GoodsStore.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+
+namespace VendingMachine.Core
+{
+    public class GoodsStore
+    {
+        private readonly Dictionary<Guid, ProductTray> _products = new Dictionary<Guid, ProductTray>();
+
+        public void TakeProductItem(Guid productId)
+        {
+            var productTray = GetProductTray(productId);
+            productTray.TakeOne();
+
+        }
+
+        [NotNull]
+        private ProductTray GetProductTray(Guid productId)
+        {
+            ProductTray productTray;
+            if (!_products.TryGetValue(productId, out productTray))
+            {
+                throw new InvalidOperationException($"Unknown product requested (Id: {productId}).");
+            }
+            return productTray;
+        }
+    }
+}
---
+++ src/VendingMachine.Core/ProductTray.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace VendingMachine.Core
+{
+    public class ProductTray
+    {
+        public ProductTray(Guid id, string name, decimal price, int count = 0)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Count = count;
+        }
+
+        public Guid Id { get; }
+        public string Name { get; }
+        public decimal Price { get; }
+        public int Count { get; private set; }
+        public bool HasItems => Count > 0;
+
+        public void TakeOne()
+        {
+            if (!HasItems)
+            {
+                throw new EmptyProductTrayException(Id, Name);
+            }
+            Count--;
+        }
+    }
+}
